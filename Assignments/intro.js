520b46e0224055e566c2737f6d1f1d507b859ea8
Lauren McNeill <lam289@pitt.edu>
2020-01-24 01:37:42 -0500

    moving any assignments done this so far into repo

---
+++ Assignments/intro.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded',init,false);
+            
+function init() {
+    alert('welcome!');
+    var buttons = document.getElementsbyTagName("button")
+buttons[0].addEventListener('click', moreInfo, false)
+}
+
+function moreInfo() {
+    
+window.location=''
+}
